a90f21ae7b57f216281ab05bce5ec7a610886c00
陳冠宇 <chen630602@gmail.com>
2024-04-26 02:52:23 +0800

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -3,35 +3,23 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../models/post");
 
-
-
 router
   .get("/", async (req, res, next) => {
     const post = await Post.find();
     handleSuccess(res, post);
   })
   .post("/", async (req, res, next) => {
-    req.on("end", async () => {
-      try {
-        const data = JSON.parse(body);
-        if (data.content !== undefined) {
-          const newPost = await Post.create({
-            name: data.name,
-            content: data.content,
-          });
-          handleSuccess(res, newPost);
-        } else {
-          const message = "欄位未填寫正確，或無該筆貼文 id";
-          handleError(res, message);
-        }
-      } catch (error) {
-        handleError(res, error);
-      }
-    });
+    try {
+      const toPost = await Post.create(req.body);
+      handleSuccess(res, toPost);
+    } catch (err) {
+      const newMessage = "資料傳輸或建立失敗";
+      handleError(res, newMessage);
+    }
   })
   .delete("/:id", async (req, res, next) => {
-    const id = req.url.split("/").pop();
     try {
+      const id = req.url.split("/").pop();
       await Post.findByIdAndDelete(id);
       handleSuccess(res, null);
     } catch (err) {
@@ -39,29 +27,19 @@ router
     }
   })
   .delete("/", async (req, res, next) => {
-    await Post.deleteMany();
-    const newMessage = "已刪除全部貼文";
-    handleSuccess(res, newMessage);
+    await Post.deleteMany({});
+    handleSuccess(res, null);
   })
   .patch("/:id", async (req, res, next) => {
-    req.on("end", async () => {
-      try {
-        const data = JSON.parse(body);
-
-        if (data !== undefined) {
-          const id = req.url.split("/").pop();
-          await Post.findByIdAndUpdate({ _id: id }, data, { new: true });
-          const newData = await Post.find({ _id: id });
-          handleSuccess(res, newData);
-        } else {
-          const message = "欄位未填寫正確，或無該筆貼文 id";
-          handleError(res, message);
-        }
-      } catch (error) {
-        const message = error;
-        handleError(res, message);
-      }
-    });
+    try {
+      const { id } = req.params;
+      const editPosts = await Post.findByIdAndUpdate(id, req.body, {
+        new: true,
+      });
+      handleSuccess(res, editPosts);
+    } catch (err) {
+      handleError(res, err);
+    }
   });
 
 module.exports = router;

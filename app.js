b006d1166b4f7dccb9e3393d7d92061282ef5e1e
陳冠宇 <chen630602@gmail.com>
2024-04-26 03:00:35 +0800

    Update app.js

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use("/", postsRouter);
 app.use("/posts", postsRouter);
 // 404 中間件
 app.use((req, res, next) => {

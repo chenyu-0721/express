8d7cc78a9264c9b253703b698fa469ae500fc39d
陳冠宇 <chen630602@gmail.com>
2024-04-27 13:22:52 +0800

    test

--- app.js
+++ app.js
@@ -1,32 +1,39 @@
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var postsRouter = require("./routes/posts");
+const port = process.env.PORT || 4000;
+var app = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+// mongoose 開始
+
+// app.js
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
-const indexRouter = require("./routes/index");
-const postsRouter = require("./routes/posts");
-
-const app = express();
-
-// 連接資料庫
 dotenv.config({ path: "./config.env" });
 const DB = process.env.DATABASE.replace(
   "<password>",
   process.env.DATABASE_PASSWORD
 );
 
-// const DB = 'mongodb://127.0.0.1:27017/homework';
+mongoose.connect(DB).then(() => console.log("資料庫連接成功"));
 
-mongoose
-  .connect(DB)
-  .then(() => {
-    console.log("資料庫連線成功");
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+// mongoose 結束
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -35,13 +42,26 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
+app.use("/users", usersRouter);
 app.use("/posts", postsRouter);
-// 404 中間件
-app.use((req, res, next) => {
-  res.status(404).json({
-    status: "success",
-    message: "無此網站路由",
-  });
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
 });
 
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
 module.exports = app;
--- routes/posts.js
+++ routes/posts.js
@@ -1,47 +1,58 @@
-// routes/posts.js
 const express = require("express");
 const router = express.Router();
-const Post = require("../models/post");
-const handleError = require("../handleError.js");
+const Post = require("../models/post.js");
 const handleSuccess = require("../handleSuccess.js");
+const handleError = require("../handleError.js");
 
-router
-  .get("/", async (req, res, next) => {
+router.get("/", async (req, res, next) => {
+  try {
     const post = await Post.find();
     handleSuccess(res, post);
-  })
-  .post("/", async (req, res, next) => {
-    try {
-      const toPost = await Post.create(req.body);
-      handleSuccess(res, toPost);
-    } catch (err) {
-      const newMessage = "資料傳輸或建立失敗";
-      handleError(res, newMessage);
-    }
-  })
-  .delete("/:id", async (req, res, next) => {
-    try {
-      const id = req.url.split("/").pop();
-      await Post.findByIdAndDelete(id);
-      handleSuccess(res, null);
-    } catch (err) {
-      handleError(res, err);
-    }
-  })
-  .delete("/", async (req, res, next) => {
+  } catch (err) {
+    const error = "取得失敗";
+    handleError(res, error);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const post = await Post.create(req.body);
+    handleSuccess(res, post);
+  } catch (err) {
+    const error = "建立失敗";
+    handleError(res, error);
+  }
+});
+
+router.patch("/:id", async (req, res, next) => {
+  try {
+    const newpost = await Post.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    handleSuccess(res, newpost);
+  } catch (err) {
+    const error = "編輯失敗";
+    handleError(res, error);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const delpost = await Post.findByIdAndDelete(req.params.id);
+    handleSuccess(res, delpost);
+  } catch (err) {
+    const error = "刪除單筆失敗";
+    handleError(res, error);
+  }
+});
+
+router.delete("/", async function (req, res, next) {
+  try {
     await Post.deleteMany({});
     handleSuccess(res, null);
-  })
-  .patch("/:id", async (req, res, next) => {
-    try {
-      const { id } = req.params;
-      const editPosts = await Post.findByIdAndUpdate(id, req.body, {
-        new: true,
-      });
-      handleSuccess(res, editPosts);
-    } catch (err) {
-      handleError(res, err);
-    }
-  });
+  } catch (err) {
+    handleError(res, err, "刪除所有貼文資料失敗");
+  }
+});
 
 module.exports = router;
